77c129321e553ea874c4aa49276dac4db2efc26c
unnati1004 <unnatigandhi999@gmail.com>
2024-06-30 12:58:20 +0530

    implement pagination with dummyjson api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [product, setProduct] = useState([]);
+  const [page, setPage] = useState(1);
+  const fetchProduct = async () => {
+    const res = await fetch("https://dummyjson.com/products?limit=100");
+    const data = await res.json();
+    if (data && data.products) {
+      setProduct(data.products);
+    }
+    console.log("data", data);
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+
+  const selectPageHandler = (selectPage)=>{
+    setPage(selectPage);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {product.length > 0 && (
+        <div className="products">
+          {product.slice(page * 10 - 10, page * 10).map((prod) => {
+            return (
+              <span className="products__single" key={prod.id}>
+                <img src={prod.thumbnail} alt={prod.title} />
+                <span>{prod.title}</span>
+              </span>
+            );
+          })}
+        </div>
+      )}
+      {product.length > 0 && <div className="pagination">
+        <span>◀</span>
+        {[...Array(product.length/10)].map((_,i)=>{
+          return(
+            <span 
+            onClick={()=>{selectPageHandler(i+1)}}
+            key={i}>{i+1}</span>
+          )
+        })
+        }
+        <span>▶</span>
+         </div> }
     </div>
   );
 }

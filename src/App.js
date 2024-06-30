dfb5ea6a3b17331b97b9d0baa1ddefc62569f180
unnati1004 <unnatigandhi999@gmail.com>
2024-06-30 16:14:19 +0530

    add pagination and skip through api

--- src/App.js
+++ src/App.js
@@ -4,28 +4,35 @@ import { useEffect, useState } from "react";
 function App() {
   const [product, setProduct] = useState([]);
   const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
   const fetchProduct = async () => {
-    const res = await fetch("https://dummyjson.com/products?limit=100");
+    const res = await fetch(
+      `https://dummyjson.com/products?limit=10&skip=${page * 10 - 10}`
+    );
     const data = await res.json();
     if (data && data.products) {
       setProduct(data.products);
+      setTotalPages(Math.ceil(data.total / 10)); // Correct totalPages calculation
     }
     console.log("data", data);
   };
 
   useEffect(() => {
     fetchProduct();
-  }, []);
+  }, [page]);
 
-  const selectPageHandler = (selectPage)=>{
-    setPage(selectPage);
-  }
+  const selectPageHandler = (selectPage) => {
+    if (selectPage >= 1 && selectPage <= totalPages && selectPage !== page) {
+      setPage(selectPage);
+    }
+  };
 
   return (
     <div className="App">
       {product.length > 0 && (
         <div className="products">
-          {product.slice(page * 10 - 10, page * 10).map((prod) => {
+          {product.map((prod) => {
             return (
               <span className="products__single" key={prod.id}>
                 <img src={prod.thumbnail} alt={prod.title} />
@@ -35,18 +42,39 @@ function App() {
           })}
         </div>
       )}
-      {product.length > 0 && <div className="pagination">
-        <span>◀</span>
-        {[...Array(product.length/10)].map((_,i)=>{
-          return(
-            <span 
-            onClick={()=>{selectPageHandler(i+1)}}
-            key={i}>{i+1}</span>
-          )
-        })
-        }
-        <span>▶</span>
-         </div> }
+      {product.length > 0 && (
+        <div className="pagination">
+          <span
+            className={page === 1 ? "pagination__disable" : ""}
+            onClick={() => {
+              selectPageHandler(page - 1);
+            }}
+          >
+            ◀
+          </span>
+          {[...Array(totalPages)].map((_, i) => {
+            return (
+              <span
+                className={page === i + 1 ? "pagination__selected" : ""}
+                onClick={() => {
+                  selectPageHandler(i + 1);
+                }}
+                key={i}
+              >
+                {i + 1}
+              </span>
+            );
+          })}
+          <span
+            className={page === totalPages ? "pagination__disable" : ""}
+            onClick={() => {
+              selectPageHandler(page + 1);
+            }}
+          >
+            ▶
+          </span>
+        </div>
+      )}
     </div>
   );
 }
